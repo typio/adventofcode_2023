0a08c2958a11270732de3620c87d27ce81ee688f
Thomas Huber <me@tohuber.com>
2023-12-03 21:44:19 -0700

    🎁

--- utils.ml
+++ utils.ml
@@ -6,6 +6,12 @@ let read_lines filename =
   in
   aux (open_in filename) []
 
+let is_digit c = c >= '0' && c <= '9'
+let listify_string s = s |> String.to_seq |> List.of_seq
+let stringify_list l = l |> List.to_seq |> String.of_seq
+let reverse_string s = stringify_list (List.rev (listify_string s))
+let stringify_char c = String.make 1 c
+
 let time_fn f x =
   let t = Sys.time() in
   let fx = f x in
