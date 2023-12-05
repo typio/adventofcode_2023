e24f1c1b401eb19a81ea1202c890059361a8816d
Thomas Huber <me@tohuber.com>
2023-12-04 17:46:43 -0700

    🛷

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 dayXX (Do not put .ml!)"
+    echo "Usage: $0 dayXX"
     exit 1
 fi
 
 DAY_FILE=$1
+
+if [[ ! $DAY_FILE =~ ^day[0-9][0-9]$ ]]; then
+    echo "Error: Argument should be in the format 'dayXX' where 'X' is a digit."
+    exit 1
+fi
 
 ocamlopt -O3 -o "$DAY_FILE" utils.ml "$DAY_FILE.ml"
 
