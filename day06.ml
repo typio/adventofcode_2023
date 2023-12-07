6d4bef84fdbeb8496861db2f7a2c52d0aa97dbd9
Thomas Huber <me@tohuber.com>
2023-12-06 21:45:10 -0700

    Day 6 🎄🎄🎄🎄🎄🎄

---
+++ day06.ml
@@ -0,0 +1,83 @@
+#mod_use "utils.ml"
+
+type race = { time : int; distance : int }
+
+let solve_races races =
+  Array.fold_left
+      (*
+    Can create formula instead of looping through!
+      d = (t - st) * st
+      st in [0, t]
+
+      t st - st st = d
+
+      st^2 - t st + d 
+
+      a = 1, b = -t, c = d
+
+      st = (t +- (sqrt t^2 - 4d)) / 2
+   *)
+    (fun 
+           acc race ->
+      let ans =
+        Float.ceil
+          ((float race.time
+           +. sqrt (float ((race.time * race.time) - (4 * race.distance))))
+          /. 2.0)
+        -. Float.floor
+             ((float race.time
+              -. sqrt (float ((race.time * race.time) - (4 * race.distance))))
+             /. 2.0)
+        -. 1.
+      in
+      Float.to_int ans * acc)
+    1 races
+
+let part1 lines =
+  let times =
+    (Utils.split_string ':' lines.(0)).(1)
+    |> Utils.split_string ' '
+    |> Array.map int_of_string_opt
+    |> Utils.array_filter_some
+  in
+
+  let distances =
+    (Utils.split_string ':' lines.(1)).(1)
+    |> Utils.split_string ' '
+    |> Array.map int_of_string_opt
+    |> Utils.array_filter_some
+  in
+
+  Array.map2 (fun t d -> { time = t; distance = d }) times distances
+  |> solve_races |> print_int
+
+let part2 lines =
+  let time =
+    (Utils.split_string ':' lines.(0)).(1)
+    |> Utils.split_string ' '
+    |> Array.fold_left
+         (fun x acc -> if String.equal (String.trim x) "" then acc else x ^ acc)
+         ""
+    |> int_of_string
+  in
+  let distance =
+    (Utils.split_string ':' lines.(1)).(1)
+    |> Utils.split_string ' '
+    |> Array.fold_left
+         (fun x acc -> if String.equal (String.trim x) "" then acc else x ^ acc)
+         ""
+    |> int_of_string
+  in
+
+  Array.make 1 { time; distance } |> solve_races |> print_int
+
+let () =
+  try
+    let lines = Utils.get_lines_arr "day06.txt" in
+    Printf.printf "Timing Part 1\n";
+    ignore (Utils.time_fn part1 lines);
+    Printf.printf "Timing Part 2\n";
+    ignore (Utils.time_fn part2 lines)
+  with e ->
+    print_endline (Printexc.to_string e);
+    raise e
